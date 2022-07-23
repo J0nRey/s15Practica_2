d28525aa20735464f48970f8267ea96c8bc0482c
Jonathan Reyes <79552541+J0nRey@users.noreply.github.com>
2022-07-22 21:50:02 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,70 @@
+/*
+11. sabiendo que una cadena de texto contiene parentesis. 
+    Crear un programa que devuelva la cadena que se encuenbtra entre los parentesis.
+
+input: 'hola soy (koder)'
+
+output: 'koder'
+
+*/
+
+/*
+12. crear una funcion que permita solicitar numeros al usuario por medio del prompt y 
+    no dejar de solicitarlos hasta que el usuario no dese ingresar mas.
+    Cada numero debe ser ingresado a una coleccion.
+    Spoler. Puede que les sirva el metodo confim()
+*/
+
+/*
+13. realizar un programa que imprima una piramide de 1 al 10 de la siguiente forma.
+
+1
+22
+333
+4444
+55555
+666666
+7777777
+88888888
+999999999
+
+*/
+
+/*
+14. realizar un programa que imprima una piramide inversa.
+
+55555
+4444
+333
+22
+1
+
+*/
+
+/*
+15. teniendo un array de numeros. Determinar por cada elemento si es multiplo de 4/3/5/2.
+
+input; [3,4,5,23,20,25,10]
+
+output:
+los numeros multiplos de 4 son : [numeros]
+los numeros multiplos de 3 son : []
+los numeros multiplos de 5 son : []
+los numeros multiplos de 2 son : []
+
+*/
+
+/*
+solicitar al usuario un nombre por medio del prompt e ingresarlo a una coleccion,
+validando que no se repita nuingun nombre.
+*/
+/*
+17. dado una coleccion de numeros obtener la suma, el elemento mayor, el elemento menor y el promedio.
+*/ 
+/*
+18. solicitar al usuario por medio del prompt una serie de numeros separados por algun caracter 
+y que cada numero se agrege a una coleccion
+*/
+/*
+19. crear un programa que permita eliminar un elemento de array
+*/
